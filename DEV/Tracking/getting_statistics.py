f61ac4483cf816ae49838f25dac974912d96fecf
liangxu <Liang Xu>
2021-04-01 13:09:05 -0400

    Finish the original program running, and ready for the new algorithm to implementation

--- DEV/Tracking/getting_statistics.py
+++ DEV/Tracking/getting_statistics.py
@@ -17,6 +17,31 @@ from pyquaternion import Quaternion
 
 import argparse
 
+def iou3d(corners1, corners2):
+    ''' Compute 3D bounding box IoU.
+
+    Input:
+        corners1: numpy array (8,3), assume up direction is negative Y
+        corners2: numpy array (8,3), assume up direction is negative Y
+    Output:
+        iou: 3D bounding box IoU
+        iou_2d: bird's eye view 2D bounding box IoU
+
+    '''
+    # corner points are in counter clockwise order
+    rect1 = [(corners1[i,0], corners1[i,2]) for i in range(3,-1,-1)]
+    rect2 = [(corners2[i,0], corners2[i,2]) for i in range(3,-1,-1)] 
+    area1 = poly_area(np.array(rect1)[:,0], np.array(rect1)[:,1])
+    area2 = poly_area(np.array(rect2)[:,0], np.array(rect2)[:,1])
+    inter, inter_area = convex_hull_intersection(rect1, rect2)
+    iou_2d = inter_area/(area1+area2-inter_area)
+    ymax = min(corners1[0,1], corners2[0,1])
+    ymin = max(corners1[4,1], corners2[4,1])
+    inter_vol = inter_area * max(0.0, ymax-ymin)
+    vol1 = box3d_vol(corners1)
+    vol2 = box3d_vol(corners2)
+    iou = inter_vol / (vol1 + vol2 - inter_vol)
+    return iou, iou_2d
 
 if __name__ == '__main__':
       # Settings.
@@ -33,4 +58,3 @@ if __name__ == '__main__':
                       help='Which distance function for matching, 3d_iou or 2d_center.')
   args = parser.parse_args()
 
-  
